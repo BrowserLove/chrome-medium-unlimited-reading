cc66e09dc7379b479790d01162307d28bf1bf760
Vaidas Bagdonas <shibalba1@gmail.com>
2018-07-14 20:31:12 +0300

    finished

---
+++ main.js
@@ -0,0 +1,24 @@
+var getMemberPreviewNotice = function() {
+  return $('main + aside');
+}
+
+var isMemberPreview = function() {
+  return !!getMemberPreviewNotice()
+};
+
+var replacePreviewWithMemberContent = function(newContent) {
+  $('.postArticle-content').html(newContent);
+}
+
+var fetchMemberContent = () => fetch(document.location)
+  .then(response => response.text())
+  .then(html => $(html).find('.postArticle-content'))
+  .then(replacePreviewWithMemberContent);
+
+$(document).ready(function(){
+  if(isMemberPreview()) {
+    getMemberPreviewNotice().hide();
+
+    fetchMemberContent();
+  }
+})
