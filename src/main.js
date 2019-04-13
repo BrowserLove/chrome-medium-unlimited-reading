ddc2a9deb425a88b7c447eca74f93b8c7e7397ac
Vaidas Bagdonas <shibalba1@gmail.com>
2019-04-13 19:29:50 +0300

    update isMedium fnc to check whether the element exists

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-const isMedium = () => document.querySelector('.siteNav-logo').getAttribute('href') === 'https://medium.com/';
+const isMedium = () => {
+  const logoElement = document.querySelector('.siteNav-logo');
+
+  return logoElement && logoElement.getAttribute('href') === 'https://medium.com/';
+}
+
 const isMemberPreview = () =>
-  document.body.innerHTML.indexOf("You read a lot") > 0
-  || document.querySelector('footer .postFade') !== null
-  || document.querySelector('main + aside .postFade') !== null;
+  document.body.innerHTML.indexOf("You read a lot") > 0 ||
+  document.querySelector('footer .postFade') !== null ||
+  document.querySelector('main + aside .postFade') !== null;
+
 const removeAnnoyingElements = () => {
   const annoyingElementSelectors = [
     'footer .postFade',
@@ -26,14 +32,12 @@ const fetchPremiumContent = callback =>
 (() => {
   if(!isMedium() || !isMemberPreview()) return;
 
-  const currentContent = document.querySelector('main');
-
   fetchPremiumContent(html => {
     let newContent = html.match(/.*<main[^>]*?>(.*)<\/main>/i);
     newContent = newContent ? (newContent[1] ? newContent[1] : newContent[0]) : null;
 
     if(newContent) {
-      currentContent.innerHTML = newContent;
+      document.querySelector('main').innerHTML = newContent;
 
       removeAnnoyingElements();
     }
